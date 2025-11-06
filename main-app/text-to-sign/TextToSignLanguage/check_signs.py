6ff4ff98da27c438cb6ee9176392e648b6193a50
laas2004 <laasyachundi@gmail.com>
2025-11-06 11:25:50 +0530

    Deleted video-call and text-to-sign folders

--- main-app/text-to-sign/TextToSignLanguage/check_signs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SIGN_PATH = r"C:\Users\laasy\OneDrive\Desktop\cst12\TextToSignLanguage\signs"
-
-files = os.listdir(SIGN_PATH)
-print("Files in signs folder:", files)
-
-videos = [os.path.splitext(f)[0].lower() for f in files if f.endswith(".mp4")]
-print("Detected video names:", videos)
